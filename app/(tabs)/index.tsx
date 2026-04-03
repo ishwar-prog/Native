e97a8cb0a8a7bf861d73c91d93db06b03f84060b
Ishwar Suthar <ishwar16suthar@gmail.com>
2026-04-03 21:57:44 +0530

    Refactor app components: update styles in index.tsx, add settings screen, and improve subscription details navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
 import "@/global.css";
-import { Text, View } from "react-native";
 import { Link } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
       <Link
@@ -27,7 +27,14 @@ export default function App() {
         Go To Sign Up
       </Link>
 
-      <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+      <Link
+        href={{
+          pathname: "/subscriptions/[id]",
+          params: { id: "spotify" },
+        }}
+      >
+        Spotify Subscription
+      </Link>
 
       <Link
         href={{
--- app/(tabs)/subscriptions/[id].tsx
+++ app/(tabs)/subscriptions/[id].tsx
@@ -1,16 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Link, useLocalSearchParams } from 'expo-router'
+import { Link, useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
 
 const SubscriptionDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
 
   return (
     <View>
-      <Text>Subscription Details: {id}  </Text>
-      <Link href="/">Go Back</Link>
+      <Text>Subscription Details: {id} </Text>
+      <Link href="/subscriptions">Go Back</Link>
     </View>
-  )
-}
+  );
+};
 
-export default SubscriptionDetails
+export default SubscriptionDetails;

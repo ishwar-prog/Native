24f6d05ffe4dd7457ce5d0df93279952e281ce20
Ishwar Suthar <ishwar16suthar@gmail.com>
2026-04-04 21:36:35 +0530

    gathered assests for the app and created tabs for the app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
+      <Text className="text-xl font-bold text-success ">
         Welcome to Nativewind!
       </Text>
       <Link
